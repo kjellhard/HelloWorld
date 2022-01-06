86bc0fd13c5a0792661bab568bed2e05b90870be
Isak Kjellhard <izze96@live.se>
2022-01-07 00:29:22 +0100

    error

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 
 int main() {
+	std::string h = "Hej";
 	for (int i = 0; i<10; i++)
-		std::cout << i << ". Hello World!\n";
+		std::cout << i << ". Hello World!\n" << h[i];
 	return 0;
 }
